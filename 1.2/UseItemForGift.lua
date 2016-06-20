1b555180a5b92a25f1507ecbebd9e644ca0d913d
Jeffro-ot <likethej3@gmail.com>
2016-06-20 17:18:26 -0400

    Create UseItemForGift.lua

---
+++ 1.2/UseItemForGift.lua
@@ -0,0 +1,9 @@
+local reward = {storage = 1000,
+    items = {(1921, 1), (1922, 1), (1923, 1)}
+}
+
+function onUse(player)
+    if player.isPlayer and player.getStorageValue(reward.storage) == 0 then
+        for c, v 
+    
+        
