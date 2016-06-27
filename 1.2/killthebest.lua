4812ab1063e2866c974c0fdcf32f494b44ad5152
Jeffro-ot <likethej3@gmail.com>
2016-06-27 12:56:08 -0400

    Not Tested
    Added configuration for message to config.

--- 1.2/killthebest.lua
+++ 1.2/killthebest.lua
@@ -2,8 +2,13 @@
 --By. Jeffro
 
 local config = {--global            --global            --player
-storage = {storagelevel = 9000, storagename = 9001, storagekill = 9002}
+storage = {storagelevel = 9000, storagename = 9001, storagekill = 9002},
+msgcon = {
+    mostwanted = "You are now the most wanted player on the server!",
+    killwanted = "You have killed the most wanted player on the server!",
+    }
 }
+    
 
 local highest = config.storage
 local playerlevel = player:getLevel()
@@ -14,12 +19,12 @@ function onLogin(login, player)
     if player:getLevel() <= game:getStorageValue(highest.storagelevel) then return true end --if player isnt highest level then cancle
     game:setStorageValue(highest.storagename, playerid) --stores players id and level if highest level
     game:setStorageValue(highest.storagelevel, playerlevel)
-    player:sendTextMessage(MESSAGE_STATUS_WARNING, "You are now the most wanted player on the server!", player:getPosition())
+    player:sendTextMessage(MESSAGE_STATUS_WARNING, config.msgcon.mostwanted, player:getPosition())
 end
 
 function onKill(player, target)
     if not target.isPlayer then return true end
     if not target:getGuid() == game:getStorageValue(highest.storagename) then return true end
     game:setStorageValue(highest.storagekill, player:getGuid())
-    player:sendTextMessage(MESSAGE_STATUS_WARNING, "You have killed the most wanted player on the server!", target:getPosition())
+    player:sendTextMessage(MESSAGE_STATUS_WARNING, config.msgcon.killwanted, target:getPosition())
 end
