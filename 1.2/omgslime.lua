7be45c28acc9d881adb7e2cbd625b25db763d144
Jeffro-ot <likethej3@gmail.com>
2016-06-27 12:42:09 -0400

    Multiplying Slime on hit

--- 1.2/omgslime.lua
+++ 1.2/omgslime.lua
@@ -4,6 +4,14 @@ function onHit(player, target, creature)
     if not player.isPlayer then return true end
     if not target.isCreature then return true end
 
-    if target:getName() == "slime" then
+    if not target:getName() == monster.name then
+        return true
+    else
         local multiply = math.random(100000)
-        if multiply
+        if multiply >= monster.chance then
+            return true
+        else
+            game:createCreature(monster.name:lower(), target:getPosition())
+        end
+    end
+end
