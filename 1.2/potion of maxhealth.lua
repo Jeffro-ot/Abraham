e4920704313eeeef90ae8bdffa2be390170a7122
Jeffro-ot <likethej3@gmail.com>
2016-06-27 21:33:24 -0400

    Haven't Tested

--- 1.2/potion of maxhealth.lua
+++ 1.2/potion of maxhealth.lua
@@ -1,1 +1,13 @@
+local config = {itemid = 1974, lvlsys = 0, level = 20}
 
+function onUse(player, item)
+    if not item:itemid == config.itemid then return true end
+    if not player.isPlayer then return true end
+    if lvlsys == 1 and player:getLevel() < config.level then return true end
+    
+    local plhealth = player:getMaxHealth()
+    local addhealth = plhealth / 0.007
+    local maxhealth = plhealth + addhealth
+    
+    player:setMaxHealth(maxhealth)
+end
