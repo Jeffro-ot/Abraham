d0aa97563e792ab20c61334994f3d83f69e58e59
Jeffro-ot <likethej3@gmail.com>
2016-07-11 14:13:43 -0400

    Basic End Script Checks
    Line 8, 9, & 10 are checks to prevent extra memory load

--- 1.2/BossReward.lua
+++ 1.2/BossReward.lua
@@ -1,3 +1,11 @@
+local config = {}
+local monster = {
+    [hellraiser] = {storage = 1000}
+}
+
 function onDeath(player, target, creature)
-if not target.isCreature then return true end
+    local name= monster[creature:getName()]
+    if not target.isCreature then return true end
+    if not creature:getName() == name then return true end
+    if player:getStorageValue(name.storage) > 0 then return true end
 end
