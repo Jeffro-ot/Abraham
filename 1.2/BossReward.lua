bd685ab07f37daee611d8452caea592745bc2c32
Jeffro-ot <likethej3@gmail.com>
2016-07-11 14:35:47 -0400

    Bug Fix
    Line 7

--- 1.2/BossReward.lua
+++ 1.2/BossReward.lua
@@ -4,7 +4,7 @@ local monster = {
 }
 
 function onDeath(player, target, creature)
-    local name= monster[creature:getName()]
+    local name = monster[creature:getName()]
     if not target.isCreature then return true end
     if not creature:getName() == name then return true end
     if player:getStorageValue(name.storage) > 0 then return true end
