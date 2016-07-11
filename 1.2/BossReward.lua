9f6ef7dba6908fb58f32b041f97e900d78ec52b2
Jeffro-ot <likethej3@gmail.com>
2016-07-11 14:41:15 -0400

    setPlayerStorageValue + Message Status
    Lines 12 and 13

--- 1.2/BossReward.lua
+++ 1.2/BossReward.lua
@@ -1,6 +1,6 @@
 local config = {}
 local monster = {
-    [hellraiser] = {storage = 1000}
+    [hellraiser] = {storage = 1000, msg = "You have managed to slay the boss!"}
 }
 
 function onDeath(player, target, creature)
@@ -9,5 +9,7 @@ function onDeath(player, target, creature)
     if not creature:getName() == name then return true end
     if player:getStorageValue(name.storage) > 0 then return true end
     
+    player:setStorageValue(name.storage, 1)
+    player:sendTextMessage(MESSAGE_STATUS_WARNING, name.msg, player:getPosition())
     
 end
